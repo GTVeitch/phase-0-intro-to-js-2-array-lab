d24668dcdded8385a43716fcab5739022045ed11
Gavin Veitch <gavintveitch@gmail.com>
2022-09-21 10:16:04 -0600

    commit tree f7eb862fe094bf81e3db9db3b7908a42c8584ed5

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push("Milo", "Otis", "Garfield");
+});
+
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+function appendCat(name) {
+    const newList = [
+        ...cats.slice(),
+        `${name}`,
+    ]
+
+    return newList
+}
+
+function prependCat(name) {
+    const newList = [
+        `${name}`,
+        ...cats.slice(),
+    ]
+
+    return newList
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length-1)
+}
+
+function removeFirstCat() {
+    return cats.slice(1, cats.length+1)
+}
